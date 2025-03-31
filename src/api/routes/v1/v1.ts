a44d65243c012d6436f289148a8b5d512d5a6d35
luansantosdevpy <luansantosdev.py@gmail.com>
2025-03-31 14:54:52 -0300

    feat: initialize new routes

--- src/api/routes/v1/v1.ts
+++ src/api/routes/v1/v1.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 
+import auth from './auth';
 import healthcheck from './healthcheck';
+import permission from './permission';
+import role from './role';
+import userRole from './userRole';
 
 export default async (): Promise<Router> => {
   const router = Router();
 
   router.use('/v1', router);
   router.use('/health-check', await healthcheck());
+  router.use('/auth', await auth())
+  router.use('/permission', await permission());
+  router.use('/role', await role());
+  router.use('/user-role', await userRole());
   return router;
 };
