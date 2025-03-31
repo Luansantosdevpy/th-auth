d3334e328e3fd096a9e134ff3597eb01b0ca85b6
luansantosdevpy <luansantosdev.py@gmail.com>
2025-03-31 18:45:15 -0300

    test: create repositories mock

---
+++ test/__mocks__/repositories.mock.ts
@@ -0,0 +1,52 @@
+export interface MockUser {
+  _id: string;
+  email: string;
+  password: string;
+  name: string;
+  organizationId: string;
+  createdAt: Date;
+}
+
+export interface MockUserRole {
+  userId: string;
+  roleId: string;
+}
+
+export interface MockRole {
+  _id: string;
+  name: string;
+}
+
+export interface MockUserAttributes {
+  userId: string;
+  phone?: string;
+  photo?: string;
+}
+
+export const createMockRepositories = () => {
+  const userRepository = {
+    findByEmail: jest.fn(),
+    createUser: jest.fn(),
+    findUserAttributes: jest.fn(),
+    assignAttributesToUser: jest.fn(),
+    findUserById: jest.fn()
+  };
+
+  const userRoleRepository = {
+    findRolesByUser: jest.fn(),
+    assignRoleToUser: jest.fn()
+  };
+
+  const roleRepository = {
+    findRoleById: jest.fn(),
+    createRole: jest.fn(),
+    findAllRoles: jest.fn(),
+    findRoleByName: jest.fn()
+  };
+
+  return {
+    userRepository,
+    userRoleRepository,
+    roleRepository,
+  };
+};
