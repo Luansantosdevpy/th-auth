25baf3f833b5de074b97a9a8ecc89af3233d09a8
luansantosdevpy <luansantosdev.py@gmail.com>
2025-03-31 15:26:49 -0300

    feat: add type T to data in response formatter

--- src/infrastructure/utils/responseFormatter.ts
+++ src/infrastructure/utils/responseFormatter.ts
@@ -1,12 +1,11 @@
-export const successResponse = (
+export const successResponse = <T = null>(
   statusCode: number,
   message: string,
-  data: any = null
-): { status: number; message: string; data: any } => {
+  data: T = null as unknown as T
+): { status: number; message: string; data: T } => {
   return {
     status: statusCode,
     message,
     data,
   };
 };
-  
